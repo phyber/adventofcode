cbcbabf8a238c0689c6f7b6c327c2496f9c7570d
David O'Rourke <david.orourke@gmail.com>
2018-12-15 07:50:44 +0000

    day03 input code

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs::File;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Collect command line args
+    let args: Vec<String> = env::args().collect();
+
+    // If we got an arg, use it as the input, otherwise use stdin
+    let mut input: Box<io::Read> = if args.len() > 1 {
+        let filename = &args[1];
+        let fh = File::open(filename).unwrap();
+        Box::new(fh)
+    }
+    else {
+        let stdin = io::stdin();
+        Box::new(stdin)
+    };
+
+    // Read the input into a buffer
+    let mut buffer = String::new();
+    input.read_to_string(&mut buffer).unwrap();
 }
