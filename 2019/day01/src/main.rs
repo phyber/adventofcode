0aef94bc66054524f3d478f79f01b9fccf974693
David O'Rourke <david.orourke@gmail.com>
2019-12-05 21:06:02 +0000

    day01: part 2

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -8,8 +8,27 @@ use std::io::{
     BufReader,
 };
 
-fn fuel_required(mass: u64) -> u64 {
-    (mass as f64 / 3.0).floor() as u64 - 2
+fn fuel_for_fuel(mass: i64) -> i64 {
+    let mut additional_fuel = 0;
+    let mut next_mass = mass;
+
+    while next_mass > 0 {
+        let required = fuel_required(next_mass);
+
+        if required < 0 {
+            break;
+        }
+
+        additional_fuel = additional_fuel + required;
+        next_mass = required as i64;
+    }
+
+    additional_fuel
+}
+
+fn fuel_required(mass: i64) -> i64 {
+    let fuel = (mass as f64 / 3.0).floor() as i64 - 2;
+    fuel
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -27,16 +46,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let reader = BufReader::new(input);
-    let mut total_fuel_required = 0;
+    let mut mass_fuel_required = 0;
+    let mut fuel_fuel_required = 0;
+    let mut line_count = 1;
 
     for line in reader.lines() {
-        let mass: u64 = line?.parse()?;
+        let mass: i64 = line?.parse()?;
         let fuel = fuel_required(mass);
+        let fuel_fuel = fuel_for_fuel(fuel as i64);
 
-        total_fuel_required = total_fuel_required + fuel;
+        println!("{line}: {mass} -> {fuel} -> {fuel_fuel}",
+            line=line_count,
+            mass=mass,
+            fuel=fuel,
+            fuel_fuel=fuel_fuel,
+        );
+
+        line_count = line_count + 1;
+
+        mass_fuel_required = mass_fuel_required + fuel;
+        fuel_fuel_required = fuel_fuel_required + fuel_fuel;
     }
 
-    println!("Fuel required: {}", total_fuel_required);
+    println!("Fuel required for base mass: {}", mass_fuel_required);
+    println!("Fuel required for fuel: {}", fuel_fuel_required);
+    println!("Fuel required total: {}", mass_fuel_required + fuel_fuel_required);
 
     Ok(())
 }
