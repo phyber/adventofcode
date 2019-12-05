0888160a32689de66c20ccbd683162ef7e9a0908
David O'Rourke <david.orourke@gmail.com>
2019-12-05 21:33:11 +0000

    day02: Add basic intcode parsing to enum

--- 2019/day02/src/main.rs
+++ 2019/day02/src/main.rs
@@ -1,9 +1,43 @@
-// day
+// day02
 use std::env;
+use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io;
+use std::str::FromStr;
 
-fn main() {
+#[derive(Debug, Clone)]
+struct IntcodeError;
+
+#[derive(Debug, Clone, PartialEq)]
+enum Intcode {
+    Finished,
+    Unknown,
+}
+
+impl fmt::Display for Intcode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Self::Finished => write!(f, "99"),
+            Self::Unknown  => write!(f, "UNKNOWN"),
+        }
+    }
+}
+
+impl FromStr for Intcode {
+    type Err = IntcodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let intcode = match s {
+            "99" => Self::Finished,
+            _    => Self::Unknown,
+        };
+
+        Ok(intcode)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     // Either read from the given file or stdin
@@ -16,4 +50,23 @@ fn main() {
         let stdin = io::stdin();
         Box::new(stdin)
     };
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_intcode_fromstr() {
+        let ic = Intcode::from_str("99").unwrap();
+        assert_eq!(ic, Intcode::Finished);
+    }
+
+    #[test]
+    fn test_intcode_display() {
+        let ic: Intcode = "99".parse().unwrap();
+        assert_eq!(ic, Intcode::Finished);
+    }
 }
