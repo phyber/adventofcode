fd5e4b0b6c5e575768e834d601e8dfa591bf964b
David O'Rourke <david.orourke@gmail.com>
2019-12-09 23:50:34 +0000

    template: Minor updates

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,11 +1,20 @@
 // day
 use std::env;
+use std::error::Error;
 use std::fs::File;
-use std::io;
+use std::io::{
+    self,
+    prelude::*,
+    BufReader,
+};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+// CLI arguments
+type Args = Vec<String>;
 
+// Get an input reader
+fn input_reader(
+    args: Args,
+) -> Result<BufReader<Box<dyn io::Read>>, Box<dyn Error>> {
     // Either read from the given file or stdin
     let input: Box<dyn io::Read> = if args.len() > 1 {
         let filename = &args[1];
@@ -16,4 +25,24 @@ fn main() {
         let stdin = io::stdin();
         Box::new(stdin)
     };
+
+    let reader = BufReader::new(input);
+
+    Ok(reader)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Args = env::args().collect();
+
+    // Get the input
+    let mut buffer = String::new();
+    let mut reader = input_reader(args)?;
+    reader.read_to_string(&mut buffer)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
 }
